3342ada1ba81917707024763f5b3d73d1a78b3e6
Moritz Thüning <moritz.thuening@gmail.com>
2022-06-10 21:13:55 +0200

    Add indexing

--- src/main.cu
+++ src/main.cu
@@ -1,7 +1,9 @@
 #include "tensor.h"
+#include <iostream>
 
 int main()
 {
-    Tensor<float>::from_scalar(0, {10, 10});
+    Tensor<float> tensor{ Tensor<float>::random_normal(1, 2, {5, 5}) };
+    std::cout << tensor[{1, 1}].offset;
     return 0;
 }
--- src/tensor.h
+++ src/tensor.h
@@ -4,24 +4,44 @@
 #include <algorithm>
 #include <random>
 
+std::random_device device;
+std::mt19937 random_number_generator{ device() };
+
 template <typename T>
 class Tensor {
 private:
-    static std::mt19937 random_number_generator;
     T* data{};
-    Tensor(const std::vector<int>& shape) : 
-        shape{ shape }, 
-        n_elements{ std::accumulate(shape.begin(), shape.end(), 1, std::multiplies<int>()) },
-        size{ n_elements * sizeof(T) } {
-            cudaMalloc(&data, size);
+    Tensor(const std::vector<int>& shape) :
+        shape{ shape },
+        rank{ shape.size() },
+        strides( rank ),
+        n_elements{ std::accumulate(shape.begin(), shape.end(), static_cast<size_t>(1), std::multiplies<size_t>()) },
+        size{ n_elements * sizeof(T) } 
+    {
+        int stride = 1;
+        for (int i = shape.size() - 1; i >= 0; --i) {
+            strides[i] = stride;
+            stride *= shape[i];
+        }
+        cudaMalloc(&data, size);
     }
 public:
-    const std::vector<int> shape{};
-    const int n_elements{};
+    std::vector<int> shape{};
+    const size_t rank{};
+    size_t offset{};
+    std::vector<int> strides{};
+    const size_t n_elements{};
     const size_t size{};
+    Tensor<T> operator[] (const std::vector<int>& indices) {
+        const size_t index{ std::inner_product(strides.begin(), strides.end(), indices.begin(), static_cast<size_t>(0)) };
+        Tensor<T> tensor{std::vector<int>(1, rank)};
+        tensor.data = data;
+        tensor.offset = index;
+        return tensor;
+    }
     static Tensor<T> from_vector(const std::vector<T>& vector, const std::vector<int>& shape) {
         const Tensor<T> tensor{ shape };
-        const T* array = (T*)malloc(tensor.size);
+        T* array = (T*)malloc(tensor.size);
         std::copy(vector.begin(), vector.end(), array);
         cudaMemcpy(tensor.data, array, tensor.size, cudaMemcpyHostToDevice);
         free(array);
@@ -29,7 +49,7 @@ public:
     }
     static Tensor<T> from_scalar(T scalar, const std::vector<int>& shape) {
         const Tensor<T> tensor{ shape };
-        const T* array = (T*)malloc(tensor.size);
+        T* array = (T*)malloc(tensor.size);
         std::fill_n(array, tensor.n_elements, scalar);
         cudaMemcpy(tensor.data, array, tensor.size, cudaMemcpyHostToDevice);
         free(array);
@@ -37,9 +57,9 @@ public:
     }
     static Tensor<T> random_uniform(T min, T max, const std::vector<int>& shape) {
         const Tensor<T> tensor{ shape };
-        const T* array = (T*)malloc(tensor.size);
-        const std::uniform_real_distribution<T> distribution{ min, max };
-        for (int i = 0; i < n_elements; ++i) {
+        T* array = (T*)malloc(tensor.size);
+        std::uniform_real_distribution<T> distribution{ min, max };
+        for (int i = 0; i < tensor.n_elements; ++i) {
             array[i] = distribution(random_number_generator);
         }
         cudaMemcpy(tensor.data, array, tensor.size, cudaMemcpyHostToDevice);
@@ -48,16 +68,13 @@ public:
     }
     static Tensor<T> random_normal(T mean, T standard_deviation, const std::vector<int>& shape) {
         const Tensor<T> tensor{ shape };
-        const T* array = (T*)malloc(tensor.size);
-        const std::normal_distribution<T> distribution{ mean, standard_deviation };
-        for (int i = 0; i < n_elements; ++i) {
+        T* array = (T*)malloc(tensor.size);
+        std::normal_distribution<T> distribution{ mean, standard_deviation };
+        for (int i = 0; i < tensor.n_elements; ++i) {
             array[i] = distribution(random_number_generator);
         }
         cudaMemcpy(tensor.data, array, tensor.size, cudaMemcpyHostToDevice);
         free(array);
-        return tensor;    
+        return tensor;
     }
 };
-
-template <typename T>
-std::mt19937 Tensor<T>::random_number_generator{ std::random_device() };
