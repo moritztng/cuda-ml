90071c0c942cd2b9d7b3831e3a129f05b8733542
Moritz Thüning <moritz.thuening@gmail.com>
2022-07-04 11:51:16 +0200

    Move header files to include

---
+++ include/cuda-ml.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "autodiff.h"
+#include "data.h"
+#include "kernels.h"
+#include "loss.h"
+#include "network.h"
+#include "optimizer.h"
+#include "tensor.h"
+#include "utils.h"
